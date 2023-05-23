91a8847bc57d8b01e36d4dd99da78e27e87545dc
Reinachan <github@reinina.moe>
2023-05-23 20:30:42 +0200

    move generic route selector to bottom

--- src/main.rs
+++ src/main.rs
@@ -75,13 +75,13 @@ async fn main() {
             get(upload).post(upload_file).put(put_upload_file),
         )
         .route("/profile", get(profile))
-        .route("/:file_name", get(get_file))
         .route("/delete", post(delete_file_route))
         .route("/files", get(all_files))
         .route("/user", get(new_user).post(create_user))
         .route("/users", get(all_users))
         .route("/delete-user", post(delete_user))
         .route("/edit-user", post(edit_user))
+        .route("/:file_name", get(get_file))
         .with_state(conn)
         .layer(DefaultBodyLimit::max(1024 * 1024 * 1024 * 20));
     //                               ^ sets max filesize to 20 GB
