b2e749b55961cb9c5834a240ffb8c620a9123db6
Reinachan <github@reinina.moe>
2025-03-12 06:46:52 +0100

    add api endpoint for editing user permissions

---
+++ src/api/put/update_user_permissions.rs
@@ -0,0 +1,23 @@
+use axum::{Extension, Json, extract::State, http::StatusCode, response::IntoResponse};
+use sqlx::{Pool, Sqlite};
+
+use crate::{
+    db::edit_user_permissions,
+    models::{User, UserWithoutPassword},
+};
+
+pub async fn update_user_permissions(
+    State(db): State<Pool<Sqlite>>,
+    Extension(user): Extension<User>,
+    Json(req_user): Json<UserWithoutPassword>,
+) -> Result<impl IntoResponse, (StatusCode, String)> {
+    // disallow users without manage user priviledges to change a user's permissions
+    if !user.permissions.manage_users {
+        return Err((
+            StatusCode::FORBIDDEN,
+            "You don't have the permissions to change user permissions".to_string(),
+        ));
+    }
+
+    edit_user_permissions(&db, req_user).await
+}
---
+++ src/db/edit_user_permissions.rs
@@ -0,0 +1,42 @@
+use axum::http::StatusCode;
+use sqlx::{Pool, Sqlite};
+
+use crate::models::UserWithoutPassword;
+
+pub async fn edit_user_permissions(
+    db: &Pool<Sqlite>,
+    user: UserWithoutPassword,
+) -> Result<(StatusCode, String), (StatusCode, String)> {
+    sqlx::query!(
+        "
+        BEGIN TRANSACTION;
+        
+        UPDATE users 
+        SET terminate = ? 
+        WHERE username = ?;
+        
+        UPDATE permissions
+        SET manage_users = ?, upload_files = ?, list_files = ?, delete_files = ?
+        WHERE username = ?;
+        
+        COMMIT;
+        ",
+        user.terminate,
+        user.username,
+        user.permissions.manage_users,
+        user.permissions.upload_files,
+        user.permissions.list_files,
+        user.permissions.delete_files,
+        user.username,
+    )
+    .execute(db)
+    .await
+    .map_err(|_| {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Couldn't edit user".to_owned(),
+        )
+    })?;
+
+    Ok((StatusCode::OK, "Updated password".to_owned()))
+}
